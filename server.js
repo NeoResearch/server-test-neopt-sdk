59387d1ffcb750d97af1cff2f92f9d4dd953e0db
Igor Machado <igor.machado@gmail.com>
2020-05-25 19:31:11 -0300

    basic test

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app = express();
 var staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
 
+// thanks to: https://stackoverflow.com/questions/50589083/typeerror-failed-to-execute-compile-on-webassembly-incorrect-response-mime
 app.listen(9123, function() {
+  console.log('Express version: ', require('express/package').version);
   console.log('listening on port 9123');
 });
